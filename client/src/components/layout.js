a5a202462e9f925f11d6241f5d4a39973f84bda7
David Telfer <davietelfer@hotmail.com>
2019-03-01 18:24:05 +0000

    added icons to footer

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Header from './components/header_footer.header';
+import Header from '../components/header_footer/header';
+import Footer from '../components/header_footer/footer';
 
 class Layout extends Component {
   render(){
@@ -9,7 +10,7 @@ class Layout extends Component {
         <div className="page_container">
           {this.props.children}
         </div>
-        FOOTER
+        <Footer/>
       </div>
     );
   }
