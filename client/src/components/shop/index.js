482b075789eb7a70c1a99da8fa0103a903421266
David Telfer <davietelfer@hotmail.com>
2019-03-15 16:16:14 +0000

    added price filter

--- client/src/components/shop/index.js
+++ client/src/components/shop/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import PageTop from '../utils/page_top';
 
+import { price } from '../utils/form/fixed_categories';
+
 import { connect } from 'react-redux';
 import { getBrands, getTypes } from '../../actions/products_actions';
 
 import CollapseCheckbox from '../utils/collapseCheckbox';
+import CollapseRadio from '../utils/collapseRadio';
 
 class Shop extends Component {
 
@@ -24,9 +27,26 @@ class Shop extends Component {
     this.props.dispatch(getTypes());
   }
 
+  handlePrice = (value) => {
+    const data = price;
+    let array = [];
+
+    for(let key in data){
+      if(data[key]._id === parseInt(value, 10)){
+        array = data[key].array
+      }
+    }
+    return array;
+  }
+
   handleFilters = (filters, category) => {
     const newFilters = {...this.state.filters}
     newFilters[category] = filters;
+
+    if(category === "price"){
+      let priceValues = this.handlePrice(filters);
+      newFilters[category] = priceValues
+    }
 
     this.setState({
       filters: newFilters
@@ -54,6 +74,12 @@ class Shop extends Component {
                 title="Brands"
                 list={products.brands}
                 handleFilters={(filters) => this.handleFilters(filters, 'brand')}
+              />
+              <CollapseRadio
+                initState={true}
+                title="Price"
+                list={price}
+                handleFilters={(filters) => this.handleFilters(filters, 'price')}
               />
             </div>
             <div className="right">
